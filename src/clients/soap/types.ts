9b3abd9b86d340065abd0e89f6f495141e5a90d5
Taylor Reece <taylor.reece@prismatic.io>
2022-01-19 16:41:58 -0600

    - Document SOAP client types and utility functions - Clean up some function overloading - Ensure SOAP components can be tested on Windows systems

--- src/clients/soap/types.ts
+++ src/clients/soap/types.ts
@@ -1,13 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any*/
 import { Connection } from "../../index";
-import { Client, IMTOMAttachments } from "soap";
+import { IMTOMAttachments } from "soap";
+
+/**
+ * SOAPConnection takes standard connection fields, and adds an optional `wsdlDefinitionUrl` field.
+ */
 export interface SOAPConnection extends Connection {
   fields: {
     [key: string]: unknown;
+    // The URL where a WSDL can be found
     wsdlDefinitionUrl?: string;
   };
 }
 
+/**
+ * This function determines if the object presented is a SOAP connection with a `wsdlDefinitionUrl` field.
+ * @param connection The connection to test
+ * @returns This function returns true if the connection is a SOAPConnection, and false otherwise.
+ */
 export const isSOAPConnection = (
   connection: unknown
 ): connection is SOAPConnection => {
@@ -16,32 +26,45 @@ export const isSOAPConnection = (
   return false;
 };
 
-export interface GetClient {
-  (connection: SOAPConnection): Promise<Client>;
-  (wsdlDefinition: string): Promise<Client>;
-}
+/**
+ * SOAP requests return a 4-tuple or 5-tuple with the response first, the XML response second, headers third, and the XML request fourth, and optional message attachments fifth.
+ */
+export declare type SOAPResponse = [any, any, any, any, IMTOMAttachments?];
+
+/**
+ * Overload the `soapRequest` function to take a variety of types of arguments.
+ */
 export interface SOAPRequest {
-  (params: RequestParams): Promise<[any, any, any, any, IMTOMAttachments?]>;
-  (params: RequestParams, methodParams: Record<string, unknown>): Promise<
-    [any, any, any, any, IMTOMAttachments?]
-  >;
-  (params: RequestParams, methodParams: undefined): Promise<
-    [any, any, any, any, IMTOMAttachments?]
-  >;
+  (params: RequestParams): Promise<SOAPResponse>;
+  (
+    params: RequestParams,
+    methodParams: Record<string, unknown>
+  ): Promise<SOAPResponse>;
+  (params: RequestParams, methodParams: undefined): Promise<SOAPResponse>;
 }
+
 export interface RequestParams {
+  // Either a SOAPConnection or WSDL definition
   wsdlParam: SOAPConnection | string;
+  // The SOAP method to invoke
   method: string;
+  // You can override the SOAP client `endpointURL` or `soapHeaders`
   overrides?: ClientOverrides;
+  // Log debug information about the SOAP request
   debug?: boolean;
 }
+
 export interface ClientOverrides {
+  // Override endpoint URL
   endpointURL?: string;
+  // An array of [SOAP Headers](https://www.w3.org/TR/2000/NOTE-SOAP-20000508/#_Toc478383497)
   soapHeaders?: unknown[];
 }
+
 export interface HeaderPayload {
   [x: string]: any;
 }
+
 export interface GenerateHeader {
   (
     wsdlParam: SOAPConnection | string,
@@ -54,15 +77,22 @@ export interface GenerateHeader {
     headerOptions: { namespace: string; xmlns: string }
   ): Promise<string>;
 }
+
 export interface BasicAuthConnection extends Connection {
   fields: {
     [key: string]: unknown;
     username: unknown;
     password: unknown;
+    // A SOAP login method, defined in the WSDL
     loginMethod: unknown;
   };
 }
 
+/**
+ * This function determines if the object presented is a Basic Auth connection with `username`, `password`, and `loginMethod` fields.
+ * @param connection The connection to test
+ * @returns This function returns true if the connection is a SOAPConnection, and false otherwise.
+ */
 export const isBasicAuthConnection = (
   connection: Connection
 ): connection is BasicAuthConnection => {
@@ -71,6 +101,7 @@ export const isBasicAuthConnection = (
     "username" in fields && "password" in fields && "loginMethod" in fields
   );
 };
+
 export interface SOAPAuth {
   (connection: BasicAuthConnection, wsdlDefinition: string): Promise<any>;
   (connection: BasicAuthConnection & SOAPConnection): Promise<any>;
