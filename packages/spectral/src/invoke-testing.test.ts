90a091a5836e9d5459b457ae6f4c99ea8f2ee697
Ryan Wersal <ryan.wersal@prismatic.io>
2022-04-21 09:03:58 -0700

    Use correct authoring type for ConnectionDefinition in testing

--- packages/spectral/src/invoke-testing.test.ts
+++ packages/spectral/src/invoke-testing.test.ts
@@ -1,5 +1,27 @@
-import { action, trigger } from ".";
-import { invoke, invokeTrigger } from "./testing";
+import { action, trigger, connection } from ".";
+import { invoke, invokeTrigger, createConnection } from "./testing";
+
+const myConnection = connection({
+  key: "myConnection",
+  label: "My Connection",
+  comments: "This is my connection",
+  inputs: {
+    username: {
+      label: "Username",
+      placeholder: "Username",
+      type: "string",
+      required: true,
+      comments: "Username for my Connection",
+    },
+    password: {
+      label: "Password",
+      placeholder: "Password",
+      type: "password",
+      required: true,
+      comments: "Password for my Connection",
+    },
+  },
+});
 
 const branchingAction = action({
   display: { label: "Branching", description: "Branching" },
@@ -18,6 +40,16 @@ const branchingTrigger = trigger({
   inputs: {},
   perform: async (context, payload) =>
     Promise.resolve({ payload, branch: "Foo" }),
+});
+
+describe("createConnection", () => {
+  it("works with connection types", () => {
+    const conn = createConnection(myConnection, {
+      username: "hello",
+      password: "world",
+    });
+    expect(conn).toBeDefined();
+  });
 });
 
 describe("invoke", () => {
