3a1cd1b74f0adb083f877f66a7214be9ce556b9b
Ryan Wersal <ryan.wersal@prismatic.io>
2023-04-10 09:16:49 -0700

    Add conditional-logic into spectral (#120)
    * Initial import of conditional logic
    
    * Update conditionalLogic to TS/eslint standards; swap dayjs for date-fns
    
    * Add isNotEmpty operator

---
+++ packages/spectral/src/conditionalLogic/index.test.ts
@@ -0,0 +1,380 @@
+import fc from "fast-check";
+import {
+  ConditionalExpression,
+  BooleanExpression,
+  BooleanOperator,
+  BinaryOperator,
+  UnaryOperator,
+  TermOperatorPhrase,
+} from "./types";
+import { isBefore, isAfter } from "date-fns";
+
+import { evaluate, contains, parseDate } from "./index";
+
+// Look for leading and trailing braces, square brackets, or quotes.
+const jsonLikeRegex = new RegExp(/^[[{"].*[\]}"]$/);
+
+const isJsonLike = (value: string): boolean => jsonLikeRegex.test(value.trim());
+
+describe("evaluate", () => {
+  const nonIntegerString = fc
+    .string()
+    // Ignore the JSON.parse edge cases (matching double quotes get stripped etc).
+    .filter((v) => !isJsonLike(v))
+    .filter((v) => Number.isNaN(Number.parseInt(v)));
+  const integerOnlyString = fc
+    .string()
+    .filter((v) => !Number.isNaN(Number.parseInt(v)));
+
+  describe("true expressions", () => {
+    const simpleTrueExpression = fc.oneof(
+      // equal
+      fc
+        .tuple(fc.integer(), fc.integer())
+        .filter(([left, right]) => left === right)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.equal,
+            left.toString(),
+            right.toString(),
+          ]
+        ),
+      // notEqual
+      fc
+        .tuple(fc.integer(), fc.integer())
+        .filter(([left, right]) => left !== right)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.notEqual,
+            left.toString(),
+            right.toString(),
+          ]
+        ),
+      // greaterThan
+      fc
+        .tuple(fc.integer(), fc.integer())
+        .filter(([left, right]) => left > right)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.greaterThan,
+            left.toString(),
+            right.toString(),
+          ]
+        ),
+      // greaterThanEqual
+      fc
+        .tuple(fc.integer(), fc.integer())
+        .filter(([left, right]) => left >= right)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.greaterThanOrEqual,
+            left.toString(),
+            right.toString(),
+          ]
+        ),
+      // lessThan
+      fc
+        .tuple(fc.integer(), fc.integer())
+        .filter(([left, right]) => left < right)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.lessThan,
+            left.toString(),
+            right.toString(),
+          ]
+        ),
+      // lessThanEqual
+      fc
+        .tuple(fc.integer(), fc.integer())
+        .filter(([left, right]) => left <= right)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.lessThanOrEqual,
+            left.toString(),
+            right.toString(),
+          ]
+        ),
+      // in
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        // Filter out whitespace only strings.
+        .filter(([left]) => left !== left.trim())
+        .map(([left, right]) => [left, `${left}${right}`])
+        // Need to re-test `right` since it may have become JSON-like.
+        .filter(([left, right]) => !isJsonLike(left) && !isJsonLike(right))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.in,
+            left,
+            right,
+          ]
+        ),
+      // notIn
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        .filter(([left, right]) => !right.includes(left))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.notIn,
+            left,
+            right,
+          ]
+        ),
+      // exactlyMatches
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        .filter(([left, right]) => right === left)
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.exactlyMatches,
+            left,
+            right,
+          ]
+        ),
+      // doesNotExactlyMatch
+      fc
+        .tuple(fc.integer(), integerOnlyString)
+        // Our JSON.parse method will convert string numbers into numbers. Avoid asserting against that situation.
+        .filter(([left, right]) => `${left}` !== right.trim())
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.doesNotExactlyMatch,
+            left,
+            right,
+          ]
+        ),
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        .filter(([left, right]) => right.startsWith(left))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.startsWith,
+            left,
+            right,
+          ]
+        ),
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        .filter(([left, right]) => !right.startsWith(left))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.doesNotStartWith,
+            left,
+            right,
+          ]
+        ),
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        .filter(([left, right]) => right.endsWith(left))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.endsWith,
+            left,
+            right,
+          ]
+        ),
+      fc
+        .tuple(nonIntegerString, nonIntegerString)
+        .filter(([left, right]) => !right.endsWith(left))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.doesNotEndWith,
+            left,
+            right,
+          ]
+        ),
+      nonIntegerString
+        .filter((left) => Boolean(left) === true)
+        .filter(
+          (left) => !["f", "false", "n", "no"].includes(left.toLowerCase())
+        )
+        .map((left): ConditionalExpression => [UnaryOperator.isTrue, left]),
+      fc
+        .falsy()
+        .filter((left) => Boolean(left) === false)
+        .map((left): ConditionalExpression => [UnaryOperator.isFalse, left]),
+      fc
+        .falsy()
+        .map(
+          (left): ConditionalExpression => [UnaryOperator.doesNotExist, left]
+        ),
+      fc
+        .tuple(fc.date(), fc.date())
+        .filter(([left, right]) => isAfter(left, right))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.dateTimeAfter,
+            left,
+            right,
+          ]
+        ),
+      fc
+        .tuple(fc.date(), fc.date())
+        .filter(([left, right]) => isBefore(left, right))
+        .map(
+          ([left, right]): ConditionalExpression => [
+            BinaryOperator.dateTimeBefore,
+            left,
+            right,
+          ]
+        ),
+      fc
+        .date()
+        .map(
+          (left): ConditionalExpression => [
+            BinaryOperator.dateTimeSame,
+            left,
+            left,
+          ]
+        )
+    );
+
+    const andExpressions = fc
+      .tuple(simpleTrueExpression, simpleTrueExpression)
+      .map(
+        ([left, right]): BooleanExpression => [BooleanOperator.and, left, right]
+      );
+
+    const orExpressions = fc
+      .tuple(simpleTrueExpression, simpleTrueExpression)
+      .map(
+        ([left, right]): BooleanExpression => [BooleanOperator.or, left, right]
+      );
+
+    const trueExpressions = fc.oneof(
+      simpleTrueExpression,
+      andExpressions,
+      orExpressions
+    );
+
+    it("should evaluate expressions", () => {
+      fc.assert(
+        fc.property(trueExpressions, (expression) =>
+          expect(evaluate(expression)).toStrictEqual(true)
+        )
+      );
+    });
+
+    it("should deep compare equality while attempting to convert types", () => {
+      expect(
+        evaluate([BinaryOperator.equal, { a: "" }, { a: false }])
+      ).toStrictEqual(true);
+    });
+
+    it("should strictly deep compare equality", () => {
+      expect(
+        evaluate([BinaryOperator.exactlyMatches, { a: "" }, { a: false }])
+      ).toStrictEqual(false);
+    });
+
+    it("should deep compare inequality while attempting to convert types", () => {
+      expect(
+        evaluate([BinaryOperator.notEqual, { a: "" }, { a: false }])
+      ).toStrictEqual(false);
+    });
+
+    it("should strictly deep compare inequality", () => {
+      expect(
+        evaluate([BinaryOperator.doesNotExactlyMatch, { a: "" }, { a: false }])
+      ).toStrictEqual(true);
+    });
+  });
+
+  // TODO: Add more coverage for false evaluations.
+  // describe("false expressions", () => {});
+
+  describe("validate", () => {
+    const invalidExpressions = fc.tuple(
+      fc.string().filter((v) => !Object.keys(TermOperatorPhrase).includes(v)),
+      fc.string(),
+      fc.string()
+    );
+
+    it("should throw error on invalid expression", () => {
+      fc.assert(
+        fc.property(invalidExpressions, (expression) =>
+          expect(() => evaluate(expression as any)).toThrow()
+        )
+      );
+    });
+
+    it("Expect isEmpty to Validate correctly", () => {
+      fc.assert(
+        fc.property(invalidExpressions, () => {
+          const res = evaluate([UnaryOperator.isEmpty, []]);
+          expect(res).toBe(true);
+        })
+      );
+      expect(evaluate([UnaryOperator.isEmpty, ""])).toStrictEqual(true);
+      expect(evaluate([UnaryOperator.isEmpty, "abc"])).toStrictEqual(false);
+      fc.assert(
+        fc.property(invalidExpressions, () => {
+          const res = evaluate([UnaryOperator.isEmpty, ["Hello ", "World"]]);
+          expect(res).toBe(false);
+        })
+      );
+    });
+  });
+
+  describe("test contains", () => {
+    test("test string contains with two strings", () => {
+      expect(contains("foo", "oo")).toStrictEqual(true);
+    });
+    test("test string contains with a numeric string and a number", () => {
+      expect(contains("123", 2)).toStrictEqual(true);
+    });
+    test("test string contains with a non-numeric string a number", () => {
+      expect(contains("foo", 2)).toStrictEqual(false);
+    });
+    test("test array contains when item in array", () => {
+      expect(contains([1, 2, 3], 2)).toStrictEqual(true);
+    });
+    test("test array contains when subarray in array", () => {
+      expect(contains([1, 2, 3, [4, 5]], [4, 5])).toStrictEqual(false);
+    });
+    test("test array contains when item not in array", () => {
+      expect(contains([1, 2, 3], 9)).toStrictEqual(false);
+    });
+    test("test 'array' contains when item in 'set'", () => {
+      expect(
+        contains([{ foo: null, bar: null }], { foo: null, bar: null })
+      ).toStrictEqual(false);
+    });
+    test("test 'set' contains when item in 'set'", () => {
+      expect(contains({ foo: null, bar: null }, "bar")).toStrictEqual(true);
+    });
+    test("test 'set' contains when item not in 'set'", () => {
+      expect(contains({ foo: null, bar: null }, "baz")).toStrictEqual(false);
+    });
+    test("test 'set' contains subset", () => {
+      expect(
+        contains({ key: { foo: null, bar: null } }, { foo: null, bar: null })
+      ).toStrictEqual(false);
+    });
+  });
+
+  describe("parseDate", () => {
+    test.each([
+      // String formats
+      { value: "2018", expected: new Date(2018, 0) },
+      { value: "2018-3-6", expected: new Date(2018, 2, 6) },
+      { value: "2018-03-06", expected: new Date(2018, 2, 6) },
+      {
+        value: "2018-03-06T12:36:36",
+        expected: new Date(2018, 2, 6, 12, 36, 36, 0),
+      },
+      {
+        value: "2018-03-06T12:36:36Z",
+        expected: new Date("2018-03-06T12:36:36Z"),
+      },
+      // Unix timestamp formats
+      { value: 1318781876, expected: new Date("1970-01-16T06:19:41.876Z") }, // seconds
+      { value: 1318781876406, expected: new Date("2011-10-16T16:17:56.406Z") }, // milliseconds
+      // Arbitrary numbers
+      { value: 1, expected: new Date("1970-01-01T00:00:00.001Z") },
+    ])("parses types into dates", ({ value, expected }) => {
+      const result = parseDate(value);
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
---
+++ packages/spectral/src/conditionalLogic/index.ts
@@ -0,0 +1,261 @@
+import {
+  ConditionalExpression,
+  BooleanOperator,
+  TermExpression,
+  BooleanExpression,
+  UnaryOperator,
+  BinaryOperator,
+} from "./types";
+import { isBefore, isAfter, parse, parseISO, isValid } from "date-fns";
+import _ from "lodash";
+
+export type ValidationResult = [boolean] | [boolean, string];
+
+export const validate = (
+  expression: ConditionalExpression
+): ValidationResult => {
+  if (!(expression instanceof Array)) {
+    return [false, `Invalid expression syntax: '${expression}'`];
+  }
+  const [operator] = expression;
+
+  if (operator in BooleanOperator) {
+    if (operator === BooleanOperator.and || operator === BooleanOperator.or) {
+      const [, ...predicates] = expression as BooleanExpression;
+
+      return predicates.reduce(
+        (previous, current) => {
+          const [valid, error] = validate(current);
+
+          if (!valid) {
+            return [valid, [previous[1], error].filter(Boolean).join(", ")];
+          }
+
+          return previous;
+        },
+        [true] as ValidationResult
+      );
+    }
+
+    return [false, `Invalid expression syntax: '${expression}'`];
+  } else if (operator in UnaryOperator || operator in BinaryOperator) {
+    return [true];
+  }
+
+  return [false, `Invalid expression syntax: '${expression}'`];
+};
+
+/** Convert stringified objects/values back to their native value, all other
+ *  values just pass through unaltered. */
+export const parseValue = (value: unknown) => {
+  try {
+    return typeof value === "string" ? JSON.parse(value) : value;
+  } catch {
+    return value;
+  }
+};
+
+export const contains = (container: unknown, containee: unknown): boolean => {
+  if (typeof container === "string") {
+    // Substring check.
+    // NOTE: JS is real fast and loose with types here, happily returning true
+    // for things like "123".includes(1), but we have to lie to TS.
+    return container.includes(`${containee}`);
+  }
+  if (typeof container === "object" && container !== null) {
+    if (Array.isArray(container)) {
+      // Array member check.
+      return container.includes(containee);
+    }
+    // Object attribute check (set membership).
+    return Object.prototype.hasOwnProperty.call(container, `${containee}`);
+  }
+
+  throw new Error("Invalid arguments set to 'contains'.");
+};
+
+export const parseDate = (value: unknown): Date => {
+  if (value instanceof Date && isValid(value)) {
+    return value;
+  }
+
+  if (typeof value === "number") {
+    return new Date(value);
+  }
+
+  if (typeof value === "string") {
+    const dateFormats = ["yyyy", "yyyy-MM-dd"];
+    for (const format of dateFormats) {
+      const parsed = parse(value, format, 0);
+      if (isValid(parsed)) {
+        return parsed;
+      }
+    }
+
+    const isoParsed = parseISO(value);
+    if (isValid(isoParsed)) {
+      return isoParsed;
+    }
+  }
+
+  throw new Error("Invalid argument type");
+};
+
+const isEqual = (left: unknown, right: unknown): boolean =>
+  left == right ||
+  _.isEqualWith(left, right, (objectA, objectB) => {
+    if (typeof objectA === "object" || typeof objectB === "object") {
+      /**
+       * `undefined` will fall back to the default isEqual behavior.
+       * @see -> (https://lodash.com/docs/4.17.15#isEqualWith)
+       */
+      return undefined;
+    }
+
+    return objectA == objectB;
+  });
+
+export const evaluate = (expression: ConditionalExpression): boolean => {
+  const [valid, message] = validate(expression);
+  if (!valid) {
+    throw new Error(message);
+  }
+
+  const [operator] = expression;
+
+  if (operator in BooleanOperator) {
+    const predicates = expression.slice(1) as ConditionalExpression[];
+    const results = predicates.map(evaluate);
+
+    switch (operator) {
+      case BooleanOperator.and:
+        return results.reduce((previous, current) => previous && current, true);
+      case BooleanOperator.or:
+        return results.reduce(
+          (previous, current) => previous || current,
+          false
+        );
+      default:
+        throw new Error(`Invalid operator: '${operator}'`);
+    }
+  } else if (operator in UnaryOperator) {
+    const [, leftTerm] = expression as TermExpression;
+    const left = parseValue(leftTerm);
+    // attempt to compare
+    try {
+      switch (operator) {
+        case UnaryOperator.isTrue:
+          if (typeof left === "string") {
+            const lowerValue = left.toLowerCase();
+            if (["t", "true", "y", "yes"].includes(lowerValue)) {
+              return true;
+            } else if (["f", "false", "n", "no"].includes(lowerValue)) {
+              return false;
+            }
+          }
+          return !!left;
+        case UnaryOperator.isFalse:
+          if (typeof left === "string") {
+            const lowerValue = left.toLowerCase();
+            if (["t", "true", "y", "yes"].includes(lowerValue)) {
+              return false;
+            } else if (["f", "false", "n", "no"].includes(lowerValue)) {
+              return true;
+            }
+          }
+          return !left;
+        case UnaryOperator.doesNotExist:
+          return [undefined, null, 0, NaN, false, ""].includes(left);
+        case UnaryOperator.exists:
+          return ![undefined, null, 0, NaN, false, ""].includes(left);
+        case UnaryOperator.isEmpty:
+          if (Array.isArray(left)) {
+            return left.length === 0;
+          }
+          // leftTerm is used here, since "123" would be cast to 123 and would not be a string
+          if (typeof leftTerm === "string") {
+            return leftTerm.length === 0;
+          }
+          throw new Error("Please provide an array or string");
+        case UnaryOperator.isNotEmpty:
+          if (Array.isArray(left)) {
+            return left.length > 0;
+          }
+          if (typeof leftTerm === "string") {
+            return leftTerm.length > 0;
+          }
+          throw new Error("Please provide an array or string");
+        default:
+          throw new Error(`Invalid operator: '${operator}'`);
+      }
+    } catch (error) {
+      throw new Error(`Incompatible comparison arguments, ${error}`);
+    }
+  } else {
+    const [, leftTerm, rightTerm] = expression as TermExpression;
+    let left: any = null;
+    let right: any = null;
+    // attempt to convert numeric, array, or object strings as json to native objects,
+    // otherwise fall back to original value
+    if (
+      operator in
+      [
+        BinaryOperator.dateTimeAfter,
+        BinaryOperator.dateTimeBefore,
+        BinaryOperator.dateTimeSame,
+      ]
+    ) {
+      left = parseDate(leftTerm);
+      right = parseDate(rightTerm);
+    } else {
+      left = parseValue(leftTerm);
+      right = parseValue(rightTerm);
+    }
+
+    // attempt to compare
+    try {
+      switch (operator) {
+        case BinaryOperator.equal:
+          return isEqual(left, right);
+        case BinaryOperator.notEqual:
+          return !isEqual(left, right);
+        case BinaryOperator.greaterThan:
+          return left > right;
+        case BinaryOperator.greaterThanOrEqual:
+          return left >= right;
+        case BinaryOperator.lessThan:
+          return left < right;
+        case BinaryOperator.lessThanOrEqual:
+          return left <= right;
+        case BinaryOperator.in:
+          return contains(right, left);
+        case BinaryOperator.notIn:
+          return !contains(right, left);
+        case BinaryOperator.exactlyMatches:
+          return left === right || _.isEqual(left, right);
+        case BinaryOperator.doesNotExactlyMatch:
+          return !(left === right || _.isEqual(left, right));
+        case BinaryOperator.startsWith:
+          return `${right}`.startsWith(`${left}`);
+        case BinaryOperator.doesNotStartWith:
+          return !`${right}`.startsWith(`${left}`);
+        case BinaryOperator.endsWith:
+          return `${right}`.endsWith(`${left}`);
+        case BinaryOperator.doesNotEndWith:
+          return !`${right}`.endsWith(`${left}`);
+        case BinaryOperator.dateTimeAfter:
+          return isAfter(left, right);
+        case BinaryOperator.dateTimeBefore:
+          return isBefore(left, right);
+        case BinaryOperator.dateTimeSame:
+          return left == right;
+        default:
+          throw new Error(`Invalid operator: '${operator}'`);
+      }
+    } catch (error) {
+      throw new Error(`Incompatible comparison arguments, ${error}`);
+    }
+  }
+};
+
+export * from "./types";
