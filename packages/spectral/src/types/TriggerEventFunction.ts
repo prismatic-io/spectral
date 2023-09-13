39085717d1e735c914b4762c8d54843c7374f66f
Justin Hipple <justin.hipple@prismatic.io>
2023-09-13 08:16:30 -0700

    Another iteration of Trigger Event Handler preview

--- packages/spectral/src/types/TriggerEventFunction.ts
+++ packages/spectral/src/types/TriggerEventFunction.ts
@@ -1,22 +1,7 @@
-import {
-  Inputs,
-  ActionInputParameters,
-  ActionLogger,
-  Customer,
-  Instance,
-  User,
-} from ".";
-
-/** Context provided to the event function containing helpers and contextual data */
-export interface TriggerEventFunctionContext {
-  logger: ActionLogger;
-  customer: Customer;
-  instance: Instance;
-  user: User;
-}
+import { Inputs, ActionContext, ActionInputParameters } from ".";
 
 /** Definition of the function to execute when a Trigger Event occurs. */
 export type TriggerEventFunction<TInputs extends Inputs> = (
-  context: TriggerEventFunctionContext,
+  context: ActionContext,
   params: ActionInputParameters<TInputs>
 ) => Promise<void>;
