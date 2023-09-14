774ebd4b76cb93a28b3e5979cf58ff10e73d4e3c
Ryan Wersal <ryan.wersal@prismatic.io>
2023-09-14 12:42:22 -0700

    Disable several rules that are overall noisy in component dev contexts

--- packages/eslint-config-spectral/src/index.ts
+++ packages/eslint-config-spectral/src/index.ts
@@ -10,8 +10,8 @@ module.exports = {
   plugins: ["@typescript-eslint", "import", "prettier"],
   extends: [
     "eslint:recommended",
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:import/recommended",
     "plugin:import/typescript",
     "plugin:prettier/recommended",
@@ -25,6 +25,14 @@ module.exports = {
   rules: {
     camelcase: "off",
     "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-base-to-string": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
   },
   settings: {
     "import/parsers": {
