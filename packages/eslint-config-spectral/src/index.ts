96d4b4644a9b59526c1a5ad3ac5ed872ee2a2bb3
Taylor Reece <taylor.reece@prismatic.io>
2024-03-05 12:37:17 -0600

    Add no-unused-vars rule to eslint

--- packages/eslint-config-spectral/src/index.ts
+++ packages/eslint-config-spectral/src/index.ts
@@ -31,6 +31,7 @@ module.exports = {
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
     "@typescript-eslint/no-base-to-string": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
   },
