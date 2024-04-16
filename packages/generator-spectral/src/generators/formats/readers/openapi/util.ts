946dfc5c676d5c84e5324e2a02db4c961b3ea42c
Ryan Wersal <ryan.wersal@prismatic.io>
2024-04-16 14:06:13 -0700

    Remove generator-spectral package as scaffolding has been moved to prism (#194)

--- packages/generator-spectral/src/generators/formats/readers/openapi/util.ts
+++
@@ -1,9 +0,0 @@
-import { camelCase } from "lodash";
-import { toWords } from "number-to-words";
-
-/** Convert path to grouping tag. */
-export const toGroupTag = (path: string): string =>
-  camelCase(path === "/" ? "root" : path.split("/")[1]).replace(
-    /^(\d+)/,
-    (_, match) => toWords(match)
-  );
