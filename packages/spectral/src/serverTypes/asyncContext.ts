3e7a9a5bb31f233f681e7741ddd1212aaca1f337
Pattra Audcharevorakul <pattra.audcharevorakul@prismatic.io>
2025-09-29 11:37:52 -0400

    Make in-src component manifests compatible with user-defined component keys

--- packages/spectral/src/serverTypes/asyncContext.ts
+++ packages/spectral/src/serverTypes/asyncContext.ts
@@ -1,15 +1,16 @@
+import { ComponentRegistry, IntegrationDefinition } from "../types";
 import type { ActionContext } from "../types/ActionPerformFunction";
 
 // Only import async_hooks in Node.js environments
 const asyncHooks = typeof window === "undefined" ? require("node:async_hooks") : null;
 const actionContextStorage = asyncHooks ? new asyncHooks.AsyncLocalStorage() : null;
+const integrationContextStorage = asyncHooks ? new asyncHooks.AsyncLocalStorage() : null;
 
 export function runWithContext<T>(
   context: ActionContext,
   fn: () => T | Promise<T>,
 ): T | Promise<T> {
   if (!actionContextStorage) {
-    // This shouldn't be running in a browser environment anyway.
     return fn();
   }
   return actionContextStorage.run(context, fn);
@@ -24,3 +25,53 @@ export function requireContext(): ActionContext {
 
   return context;
 }
+
+export function runWithIntegrationContext<T extends IntegrationDefinition, U>(
+  context: T,
+  fn: () => U,
+): U {
+  if (!integrationContextStorage) {
+    console.warn(
+      "Creating integration without context. This may result in errors when generating component manifests.",
+    );
+
+    return fn();
+  }
+
+  return integrationContextStorage.run(context, fn);
+}
+
+export function requireIntegrationContext<T extends IntegrationDefinition>(): T {
+  const context = integrationContextStorage.getStore();
+
+  if (!context) {
+    throw new Error(
+      "IntegrationContext not found. Ensure this code is wrapped via runWithIntegrationContext.",
+    );
+  }
+
+  return context;
+}
+
+export const findUserDefinedComponentKey = <T extends ComponentRegistry>(
+  componentKey: string,
+  registry?: T,
+): keyof T => {
+  if (!registry) {
+    throw new Error(
+      "Error locating component registry. Do you have a component registry defined on your integration?",
+    );
+  }
+
+  const userKey = Object.keys(registry).find((userKey) => {
+    return registry[userKey].key === componentKey;
+  });
+
+  if (!userKey) {
+    throw new Error(
+      `Error locating component ${componentKey} in the component registry. Is this component properly installed?`,
+    );
+  }
+
+  return userKey;
+};
