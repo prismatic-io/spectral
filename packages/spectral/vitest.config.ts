f7e26a05f64b4c8f12b7633b4c960ff2d1e0416d
Ryan Wersal <ryan.wersal@prismatic.io>
2026-07-31 12:01:15 -0700

    Standardize Vitest test file naming

--- packages/spectral/vitest.config.ts
+++ packages/spectral/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
+    include: ["**/*.test.ts"],
     exclude: ["dist/**", "node_modules/**"],
     typecheck: {
       tsconfig: "./tsconfig.test.json",
